ef96ecaf0bf15ee9cdc1839aee85a0f30ca5d625
diogenesoftoronto <diogenesoft@protonmail.com>
2021-12-02 17:02:36 -0500

    example edu not working but other sites are this will be a placeholder for better examples refactor to different port if you want it to work with https sites

--- requestExample.js
+++ requestExample.js
@@ -1,6 +1,6 @@
 const request = require('request')
 
-const url = 'https://github.com/'
+const url = 'https://www.example.edu/'
 request(url, function callin(error, response, body) {
   console.log('error: ', error, 'response: ', response)
   setTimeout(function () {
@@ -10,3 +10,16 @@ request(url, function callin(error, response, body) {
 
   }, 1200)
 })
+const net = require('net');
+const conn = net.createConnection({ 
+  host: url,
+  port: 80
+});
+conn.setEncoding('UTF8');
+conn.on('connect', () => {
+  console.log(`Connected to server!`);
+
+  conn.write(`GET / HTTP/1.1\r\n`);
+  conn.write(`Host: example.edu\r\n`);
+  conn.write(`\r\n`);
+});
