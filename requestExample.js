598f7d62859fb786e449d9f151d42e51cb238512
diogenesoftoronto <diogenesoft@protonmail.com>
2021-12-02 16:21:28 -0500

    IT WORKS

--- requestExample.js
+++ requestExample.js
@@ -1,3 +1,12 @@
 const request = require('request')
 
-request()
+const url = 'https://github.com/'
+request(url, function callin(error, response, body) {
+  console.log('error: ', error, 'response: ', response)
+  setTimeout(function () {
+    console.log("here comes the rest of the page...")
+    console.log('\n')
+    console.log(body)
+
+  }, 1200)
+})
